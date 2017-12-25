7eacf6415545ebbfc847c5cfd990c665ee59ca71
Alex <dogrock2@Alexanders-MacBook-Pro.local>
2017-12-25 14:16:08 -0500

    setting mongodb2

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
+let databaseUri = "mongodb://localhost/scrapeNYTimes";
+
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/scrapeNYTimes", {
-    useMongoClient: true
-});
+
+if(process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);   
+}
 
 require("./routes/routes.js")(app);
 
